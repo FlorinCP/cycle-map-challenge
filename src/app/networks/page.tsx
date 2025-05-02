ee8dc2dff0de18523498897515a8cfebafae5d0f
FlorinCP <florincosmin.peana@ulbsibiu.ro>
2025-05-02 09:30:28 +0200

    redirect to /networks

--- src/app/networks/page.tsx
+++ src/app/networks/page.tsx
@@ -5,7 +5,7 @@ import NetworksMap from '@/components/map/networks-map';
 export default function NetworksPage() {
   return (
     <Suspense fallback={<div className="h-full w-full">Loading...</div>}>
-      <div className="grid grid-cols-1 md:grid-cols-[30%_70%] min-h-screen max-h-screen w-full scrollbar-hide">
+      <div className="grid grid-cols-1 md:grid-cols-[30%_70%] min-h-screen max-h-screen w-full overflow-x-hidden">
         <NetworksListView />
         <NetworksMap
           initialLatitude={45}
